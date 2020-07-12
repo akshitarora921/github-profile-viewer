02caeaa126c84774761ab02710c4ac8945d96364
Akshit Arora <akshitarora9211@gmail.com>
2020-07-12 13:56:44 +0530

    Ui completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,91 +1,111 @@
-import React, { useState, useRef, useCallback } from "react";
-import { Link } from "react-router-dom";
-import fetchData from "../api/FetchData";
-import { Container, Row, Col, Card,CardColumns } from "react-bootstrap";
+import React, { useState, useRef, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import fetchData from '../api/FetchData';
+import { Container, Row, Col } from 'react-bootstrap';
 import './Home.css';
 
 const Home = () => {
-  const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
+	const [query, setQuery] = useState('');
+	const [pageNumber, setPageNumber] = useState(1);
 
-  const { loading, error, profiles, hasMore } = fetchData(query, pageNumber);
+	
+	const { loading, profiles, hasMore } = fetchData(query, pageNumber);
 
-  const observer = useRef();
-  const lastProfileref = useCallback(
-    (node) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPageNumber((prevPageNumber) => prevPageNumber + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [loading, hasMore]
-  );
+	const observer = useRef();
+	const lastProfileref = useCallback(
+		(node) => {
+			if (loading) return;
+			if (observer.current) observer.current.disconnect();
+			observer.current = new IntersectionObserver((entries) => {
+				if (entries[0].isIntersecting && hasMore) {
+					setPageNumber((prevPageNumber) => prevPageNumber + 1);
+				}
+			});
+			if (node) observer.current.observe(node);
+		},
+		[loading, hasMore]
+	);
 
-  function handleSearch(e) {
-    setQuery(e.target.value);
-    setPageNumber(1);
-  }
+	function handleSearch(e) {
+		setQuery(e.target.value);
+		setPageNumber(1);
+	}
 
-  return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Col>
-            <img
-              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kjCUP06WDUMR88i5wo2SqwHaHa%26pid%3DApi&f=1"
-              alt="logo"
-              height="50px"
-              width="50px"
-            ></img>
-          </Col>
-          <Col>
-            <h2>Github Profile Viewer</h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <input type="text" value={query} onChange={handleSearch}></input>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-          <CardColumns>
-            {profiles.map((profile, index) => {
-              if (profiles.length === index + 1)
-                return (
-                  <div ref={lastProfileref} key={profile}>
-                    {profile.login}
-                  </div>
-                );
-              else {
-                return (
-                    <Card style={{ width: "18rem" }}>
-                  <Link className='users' to={`/${profile.id}`} key={index}>
-                      <Card.Img variant="top" src={profile.avatar_url} height=
-                      'auto' width='100px' />
-                      <Card.Body>
-                        <Card.Title>{profile.login}</Card.Title>
-                        <Card.Text>
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
-                        </Card.Text>
-                      </Card.Body>
-                  </Link>
-                    </Card>
-                );
-              }
-            })}
-            </CardColumns>
-            <div>{loading && "loading.."}</div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className='main-container1'>
+					<div className='header'>
+						<img
+							src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kjCUP06WDUMR88i5wo2SqwHaHa%26pid%3DApi&f=1"
+							alt="logo"
+							height="50px"
+							width="50px"
+						></img>
+				
+						<h2 className='mt-2 ml-1'>Github Profile Viewer</h2>
+					</div>
+				
+				
+					<div>
+						<input
+							className="search"
+							placeholder="User Name"
+							type="text"
+							value={query}
+							onChange={handleSearch}
+						></input>
+					</div>
+				
+				
+						<Container fluid>
+							<Row className="justify-content-md-center">
+								{profiles.map((profile, index) => {
+									if (profiles.length === index + 1)
+										return (
+											<Link ref={lastProfileref} to={`/${profile.id}`} key={index}>
+												<Col md="12" className="profile">
+													<Row>
+														<Col md="auto" xs="auto">
+															<img
+																src={profile.avatar_url}
+																alt="repo"
+																className="profileimage"
+															></img>
+														</Col>
+														<Col md="auto" xs="auto">
+															<h4 className='mt-4'>{profile.login}</h4>
+														</Col>
+													</Row>
+												</Col>
+											</Link>
+										);
+									else {
+										return (
+											<Link to={`/${profile.id}`} key={index}>
+												<Col md="12" className="profile">
+													<Row>
+														<Col md="auto" xs="auto">
+															<img
+																src={profile.avatar_url}
+																alt="repo"
+																className="profileimage"
+															></img>
+														</Col>
+														<Col md="auto" xs="auto">
+															<h4 className='mt-4'>{profile.login}</h4>
+														</Col>
+													</Row>
+												</Col>
+											</Link>
+										);
+									}
+								})}
+							</Row>
+						</Container>
+			</div>
+			<div>{!loading && 'loading..'}</div>
+		</div>
+	);
 };
 
 export default Home;
--- src/api/FetchData.js
+++ src/api/FetchData.js
@@ -4,7 +4,6 @@ import Axios from 'axios'
 // const perPage =5
 export default function FetchData(query, pageNumber) {
     const [loading, setLoading]=useState(true)
-    const [error, setError]=useState(false)
     const [profiles, setProfiles]=useState([])
     const [hasMore, setHasMore]=useState(false)
 
@@ -17,7 +16,7 @@ export default function FetchData(query, pageNumber) {
         Axios({
             method:'get',
             url:'https://api.github.com/search/users',
-            params:{q:query, page:pageNumber, per_page:100},
+            params:{q:query, page:pageNumber, per_page:20},
             cancelToken:new Axios.CancelToken(c=> cancel=c)
         })
         .then(res=>{
@@ -26,14 +25,11 @@ export default function FetchData(query, pageNumber) {
             })
             setHasMore(res.data.items.length>0)
             setLoading(false)
-            // console.log(res.data)
         })
         .catch(e=>{
             if (Axios.isCancel(e)) return
-            console.log(e)
-            setError(true)
         })
         return()=>cancel()
     },[query, pageNumber])
-    return { loading,error, profiles, hasMore}
+    return { loading, profiles, hasMore}
 }
